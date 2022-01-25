e19681f2bd6921239b7d7c0f61c23ef206854451
Zhi-Mao Teng <zhimao.teng@gmail.com>
2022-01-25 21:33:02 +0800

    It generates the topics which were subscribed by consumers

--- src/main/java/TestPartition/CreateString.java
+++ src/main/java/TestPartition/CreateString.java
@@ -1,2 +1,24 @@
-package TestPartition;public class CreateString {
+package TestPartition;
+
+public class CreateString {
+    private String strForTesting ="";
+    private String str = "";
+    private String temp = "";
+
+    public String createTest(int start, int end){
+        for(int i = start; i<=end ; i++){
+            str = '"'+"test" + i + '"';
+            temp = str+",";
+            strForTesting = temp + strForTesting;
+        }
+        return strForTesting;
+    }
+
+    public static void main(String[] args) {
+        CreateString cs = new CreateString();
+        String haha = null;
+        haha = cs.createTest(1,25);
+        System.out.println(haha);
+    }
+
 }
