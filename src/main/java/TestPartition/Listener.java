3cbbc74d7dc49cf915ea2c13d7ff67554f62856b
Zhi-Mao Teng <zhimao.teng@gmail.com>
2022-01-25 21:31:21 +0800

    add seekToBeginning

--- src/main/java/TestPartition/Listener.java
+++ src/main/java/TestPartition/Listener.java
@@ -1,7 +1,14 @@
 package TestPartition;
 import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 public class Listener implements ConsumerRebalanceListener{
@@ -9,11 +16,16 @@ public class Listener implements ConsumerRebalanceListener{
     private double endTime;
     private double execTime;
     private String consumerID;
-    Listener(Args argument){
+    private KafkaConsumer<String, String> consumer;
+    private String recordTime;
+
+    Listener(Args argument,KafkaConsumer<String, String> consumer){
         this.consumerID = argument.consumerID;
+        this.consumer=consumer;
     }
     @Override
     public void onPartitionsAssigned(Collection<TopicPartition> partitions){
+        consumer.seekToBeginning(partitions);
         endTime = System.currentTimeMillis();
         System.out.println("finish rebalanced");
         calculateExecuteTime();
@@ -22,13 +34,22 @@ public class Listener implements ConsumerRebalanceListener{
     @Override
     public void onPartitionsRevoked(Collection<TopicPartition> partitions){
         startTime = System.currentTimeMillis();
-        System.out.println("Rebalanced gogo");
+        System.out.println("Rebalanced start");
     }
 
     public void calculateExecuteTime(){
         execTime = (endTime - startTime)/1000;
-        System.out.println(this.consumerID + "execution time = " + execTime);
+        recordTime = this.consumerID + "'s execution time of rebalanced = " + execTime;
+        System.out.println(recordTime);
     }
-
+    public void writeTimeIntoFile(String context) throws IOException {
+        LocalDateTime date = LocalDateTime.now();
+        File fileName = new File("/home/harry/Desktop/"+date+".txt");
+        fileName.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+        writer.write(context+"\r\n");
+        writer.flush();
+        writer.close();
+    }
 
 }
